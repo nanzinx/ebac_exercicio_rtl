d15b84b0040748ee3b2f93287069f2c776c0c284
Renan Leite <renanleites182@gmail.com>
2025-07-02 17:27:02 -0300

    criando o teste de inserção dos dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+
+const comments = [
+    { id: 1, comment: "Bala demais fi" },
+    { id: 2, comment: "Forte" }
+];
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    render(<PostComment />);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Must add two comments', () => {
+        const { debug } = render(<PostComment />);
+
+    for (let i = 0; i < comments.length; i++) {
+        const textArea = screen.getByRole('textbox');
+
+        fireEvent.change(textArea, { target: { value: comments[i].comment } });
+
+        fireEvent.click(screen.getByTestId('btn-add-comment'));
+    }
+        const renderedComments = screen.getAllByRole('listitem'); 
+
+    expect(renderedComments).toHaveLength(comments.length);
     });
 });
